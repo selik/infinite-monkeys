2bddf1e19c90b1f5594523e3f8652394cf678e83
Michael Selik <mike@selik.org>
2017-04-03 05:03:38 -0400

    refactor in an object-oriented style
    Having tested the idea on one play, I scaled up to the complete works. I
    added a multithreaded downloader and organized the Markov chain
    functions into a class.

--- README.rst
+++ README.rst
@@ -3,39 +3,34 @@ Infinite Monkeys
 ########################################################################
 
 The `infinite monkey theorem`_ suggests that a monkey pressing keys
-randomly at a keyboard will, eventually, write the complete works of
-William Shakespeare.
+randomly at a keyboard will, eventually, write the `Complete Works of
+William Shakespeare`_.
 
-Simple containers (such as the Python ``list`` and ``dict``) can be
-composed to implement complex data structures and algorithms. A classic
-example is a Markov Chain that can be trained on a corpus of text to
-randomly generate new sentences. Collecting the works of `William
-Shakespeare from MIT`_, we can simulate infinite monkeys and create
-mildly-realistic Shakespearean verse.
+.. pull-quote::
+
+   "Words, words, words." -- Hamlet_
+
+We can simulate infinite monkeys as a Markov chain and create mildly
+realistic Shakespearean verse. To increase the realism, you can increase
+the ``WordChain``'s ``memory``. On relatively small datasets like
+Shakespeare's, the chain rapidly becomes a random quote selector rather
+than a simulator.
+
+============  ==========  ======================================================
+Memory        Randomness  Example generated sentence
+============  ==========  ======================================================
+Last word     44.65%      Yea and could meet him I am your valour preys on you.
+Last 2 words  17.08%      Hath raised in me But as the day of doom.
+Last 3 words  5.03%       Which of you saw Sir Eglamour of late?
+Last 4 words  0.88%       We have not spoke us yet of torchbearers.
+============  ==========  ======================================================
+
+"Randomness" is the percent of states in the chain with more than 1 transition.
 
 
 .. _`infinite monkey theorem`: https://en.wikipedia.org/wiki/Infinite_monkey_theorem
 
-.. _`William Shakespeare from MIT`: http://shakespeare.mit.edu/
-
-
-------------------------------------------------------------------------
-The Tragedy of Hamlet
-------------------------------------------------------------------------
-
-.. pull-quote::
-
-	"Words, words, words." -- Hamlet_
-
-1. Collect the text of "Hamlet" from MIT's website.
-2. Parse dialogue from the HTML-formatted play.
-3. Train a Markov chain with a 2-word memory.
-4. Randomly generate a sentence.
-
-Comment-out any sections you don't wish to repeat if running this script
-multiple times. It's impolite to frequently and repeatedly download the
-same file from the MIT website.
-
+.. _`Complete Works of William Shakespeare`: http://shakespeare.mit.edu/
 
 .. _Hamlet: http://shakespeare.mit.edu/hamlet/hamlet.2.2.html#speech52
 
--- monkeys.py
+++ monkeys.py
@@ -1,92 +1,209 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
-
-Train a Markov chain with a 2-word memory.
-Randomly generate a sentence.
+Monkeys (re)writing Shakespeare.
 
     $ python monkeys.py
-    Might, by the image of my most painted word: O heavy burthen!
+    And dull sun not yet drunk a hundred several times.
 
 '''
+# The Complete Works of William Shakespeare is available from MIT.
+# 'http://shakespeare.mit.edu/'
 
 from collections import defaultdict
-from itertools import islice
-from urllib.request import urlopen
-import random, re
+from collections.abc import Mapping
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from gzip import GzipFile
+from io import BytesIO
+from urllib.request import Request, urlopen
+import itertools, random, re, logging
 
 
-def download(url='http://shakespeare.mit.edu/hamlet/full.html'):
+host = 'http://shakespeare.mit.edu/'
+
+
+class Chain(Mapping):
     '''
-    Download "Hamlet" in HTML-format from MIT's website.
+    A Markov chain encodes the probabilities of transitions from one
+    state to another.
     '''
-    with urlopen(url) as response:
-        data = response.read()
-    return data.decode('utf-8')
+
+    def __init__(self, sequence=None, memory=1):
+        '''
+        The ``memory`` sets the number of states considered in the
+        transition to the next state. A "memoryless" chain, that only
+        considers the current state, would have a memory of 1.
+        '''
+        if memory < 1:
+            raise ValueError('Memory must be larger than 0.')
+        self.chain = defaultdict(list)
+        self.memory = memory
+        if sequence is not None:
+            self.train(sequence)
+
+    def train(self, sequence):
+        '''
+        Train on a sequence of events
+        '''
+        it = iter(sequence)
+        history = tuple(itertools.islice(it, self.memory))
+        if len(history) < self.memory:
+            raise ValueError('Sequence must be longer than memory size')
+        for event in it:
+            self.chain[history].append(event)
+            history = history[1:] + (event,)
+
+    def walk(self, start=None):
+        '''
+        Walk randomly through the chain.
+        '''
+        if start is None:
+            history = random.choice(list(self.chain))
+            for event in history:
+                yield event
+        else:
+            history = start
+        while True:
+            event = random.choice(self.chain[history])
+            yield event
+            history = history[1:] + (event,)
+
+    def __len__(self):
+        '''
+        Number of states in the chain.
+        A state is a tuple the same size as the chain's memory.
+        '''
+        return len(self.chain)
+
+    def __getitem__(self, state):
+        '''
+        Get a state's possible transitions.
+        A state is a tuple the same size as the chain's memory.
+        '''
+        if state not in self.chain:
+            raise KeyError(state)
+        return self.chain[state]
+
+    def __iter__(self):
+        '''
+        Iterator of the chain's states.
+        A state is a tuple the same size as the chain's memory.
+        '''
+        return iter(self.chain)
+
+    @property
+    def randomness(self):
+        '''
+        Proportion of states that have more than 1 possible transition.
+        '''
+        count = len([k for k, v in self.chain.items() if len(set(v)) > 1])
+        return count / len(self)
+
+    @property
+    def events(self):
+        '''
+        All events in the chain. Some may not be reachable by a walk.
+        '''
+        states = set(itertools.chain.from_iterable(self.chain))
+        states.union(set(itertools.chain.from_iterable(self.chain.values())))
+        return states
 
 
-def parse_words(html, pattern=r'<A NAME=(\d+)\.(\d+)\.(\d+)>(.*)</A><br>'):
+class WordChain(Chain):
+    '''
+    A Markov chain of words, suitable for generating novel sentences.
+    '''
+
+    def __init__(self, words, memory=1):
+        '''
+        Train the chain and stash the capitalized words for starting
+        sentences.
+        '''
+        super().__init__(words, memory)
+        self.capitalized = [words for words in self.chain
+                            if words[0][0].isupper()]
+
+    def sentence(self):
+        '''
+        Sample a random sentence from the chain.
+        '''
+        sentence = []
+        start = random.choice(self.capitalized)
+        for word in itertools.chain(start, self.walk(start)):
+            sentence.append(word)
+            if word[-1] in '.?!':
+                return ' '.join(sentence)
+
+    @staticmethod
+    def words_from_text(lines):
+        '''
+        Parse words from lines of text.
+        '''
+        for line in lines:
+            for word in line.split():
+                yield word
+
+
+def fetchone(url):
+    '''
+    Fetch content of a URL; decompress if needed
+    '''
+    request = Request(url)
+    request.add_header('User-Agent', "infinite-monkeys")
+    request.add_header('Accept-Encoding', 'gzip')
+    with urlopen(request) as response:
+        content = response.read()
+    if response.getheader('Content-Encoding') == 'gzip':
+        content = GzipFile(fileobj=BytesIO(content), mode='rb').read()
+    logging.warning(f'Downloaded {url}')
+    return content
+
+
+def fetchmany(urls, n_threads=10, unordered=False):
+    '''
+    Fetch the content of many urls
+    '''
+    with ThreadPoolExecutor(n_threads) as pool:
+        if unordered:
+            futures = (pool.submit(fetchone, url) for url in urls)
+            for future in as_completed(futures):
+                content = future.result()
+                yield content.decode('utf-8')
+        else:
+            for content in pool.map(fetchone, urls):
+                yield content.decode('utf-8')
+
+
+def parse_dialogue(html, pattern=r'<A NAME=(\d+)\.(\d+)\.(\d+)>(.*)</A><br>'):
     '''
     Parse words of dialogue from an HTML-formatted play.
     '''
-    with open('data/hamlet.html') as f:
-        for line in f:
-            mo = re.search(pattern, line)
-            if mo is not None:
-                speech = mo[4]
-                for word in speech.split():
-                    yield word
+    for mo in re.finditer(pattern, html):
+        n_act, n_scene, n_line, speech = mo.groups()
+        yield speech
 
 
-def build_chain(words, memory=1):
+def readlines(datafile='data/combined.html'):
     '''
-    Build a Markov chain from words.
-    By default, remember only 1 previous word.
+    Lines of dialogue from the complete works of Shakespeare.
     '''
-    chain = defaultdict(list)
-    last = tuple(islice(words, memory))
-    for term in words:
-        chain[last].append(term)
-        last = last[1:] + (term,)
-    return chain
-
-
-def sample_chain(chain, start=None):
-    '''
-    Walk randomly through the chain.
-    '''
-    if start is None:
-        last = random.choice(list(chain))
-    else:
-        last = start
-    while True:
-        term = random.choice(chain[last])
-        yield term
-        last = last[1:] + (term,)
+    try:
+        with open(datafile) as f:
+            plays = f.read()
+    except FileNotFoundError as e:
+        logging.error(f'Did not find "{datafile}", downloading...')
+        logging.exception(e)
+        index = fetchone(host).decode('utf-8')
+        links = re.findall(r'<a href="(.*?)/index.html">.*?</a>', index, re.DOTALL)
+        urls = (host + href + '/full.html' for href in links)
+        plays = '\n'.join(fetchmany(urls, unordered=True))
+        with open(datafile, 'w') as f:
+            f.write(plays)
+        logging.error(f'... Saved data to "{datafile}."')
+    return parse_dialogue(plays)
 
 
 if __name__ == '__main__':
-    filename = 'data/hamlet.html'
-    try:
-        with open(filename) as f:
-            html = f.read()
-    except FileNotFoundError as e:
-        logging.error(f'Did not find "{filename}", downloading...')
-        logging.exception(e)
-        html = download()
-        with open(filename, 'w') as f:
-            f.write(html)
-        logging.error(f'... Saved HTML file as "{filename}."')
-
-    chain = build_chain(parse_words(html), memory=2)
-
-    capitalized = [words for words in chain if words[0][0].isupper()]
-    start = random.choice(capitalized)
-
-    for word in start:
-        print(word, end=' ')
-    for word in sample_chain(chain, start):
-        print(word, end=' ')
-        if word[-1] in '.?!':
-            print()
-            break
+    words = WordChain.words_from_text(readlines('data/combined.html'))
+    chain = WordChain(words, memory=2)
+    print(chain.sentence())
